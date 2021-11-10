acc89d6bc318b611575bcc4b66b93e9bb629e36e
Helen Astashina <helen.astashina@gmail.com>
2021-11-10 15:58:06 +0300

    исправляет ошибки

--- js/generator.js
+++ js/generator.js
@@ -24,10 +24,10 @@ const OFFER_TYPE = [
 ];
 
 const similarListElement = document.querySelector('.map__canvas');
-const similarCardTemplate = document.querySelector('#card').content.querySelector('.popup');
+const similarCardTemplateElement = document.querySelector('#card').content.querySelector('.popup');
 
 offersList.forEach((offer) => {
-  const cardElement = similarCardTemplate.cloneNode(true);
+  const cardElement = similarCardTemplateElement.cloneNode(true);
   cardElement.querySelector('.popup__title').textContent = offer.title;
   cardElement.querySelector('.popup__text--address').textContent = offer.address;
   cardElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
@@ -35,8 +35,8 @@ offersList.forEach((offer) => {
   cardElement.querySelector('.popup__type').textContent = cardOfferType.name;
   cardElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
   cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-  const featuresContainer = cardElement.querySelector('.popup__features');
-  const featuresList = featuresContainer.querySelectorAll('.popup__feature');
+  const featuresContainerElement = cardElement.querySelector('.popup__features');
+  const featuresList = featuresContainerElement.querySelectorAll('.popup__feature');
   const modifiers = offer.features.map((feature) => `popup__feature--${  feature}`);
 
   featuresList.forEach((featuresListItem) => {
@@ -47,13 +47,13 @@ offersList.forEach((offer) => {
     }
   });
   cardElement.querySelector('.popup__description').textContent = offer.description;
-  const photosContainer = cardElement.querySelector('.popup__photos');
-  const photosTemplate = photosContainer.querySelector('.popup__photo');
-  photosContainer.innerHTML = '';
+  const photosContainerElement = cardElement.querySelector('.popup__photos');
+  const photosTemplateElement = photosContainerElement.querySelector('.popup__photo');
+  photosContainerElement.innerText = '';
   offer.photos.forEach((photo) => {
-    const photosElement = photosTemplate.cloneNode(true);
+    const photosElement = photosTemplateElement.cloneNode(true);
     photosElement.src = photo;
-    photosContainer.appendChild(photosElement);
+    photosContainerElement.appendChild(photosElement);
   });
   cardElement.querySelector('.popup__avatar').src = offer.author.avatar;
   similarListElement.appendChild(cardElement);
