1371f332f4907614243148883ca7b41309bac4e9
Helen Astashina <helen.astashina@gmail.com>
2021-11-17 21:15:22 +0300

    подготовка

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
 import './form.js';
-import './map.js';
+import {debounce} from './utils/debounce.js';
+import {renderOfferPin, resetMap} from './map.js';
+import {setTypeFilter, setPriceFilter, setRoomsFilter, setGuestsFilter, setFeaturesFilter} from './filter-form.js';
+import {getData} from './api.js';
+import {setClearForm} from './form.js';
+
+getData((offers) => {
+  renderOfferPin(offers);
+  setTypeFilter(
+    debounce(
+      () => renderOfferPin(offers),
+    ));
+  setPriceFilter(
+    debounce(
+      () => renderOfferPin(offers),
+    ));
+  setRoomsFilter(
+    debounce(
+      () => renderOfferPin(offers),
+    ));
+  setGuestsFilter(
+    debounce(
+      () => renderOfferPin(offers),
+    ));
+
+  setFeaturesFilter(
+    debounce(
+      () => renderOfferPin(offers),
+    ));
+
+  setClearForm(() => resetMap(offers));
+});
